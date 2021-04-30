9dbeaf145a2b2d2c03c5eacef1e01ab1cf0f396e
Michael Sandrof <msandrof@jamcity.com>
2021-04-30 12:56:33 -0700

    Add cache cleanup to cleanup.sh

--- sbt-unity/cleanup.sh
+++ sbt-unity/cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Remove build artifacts to prevent some permissioning issues
 [[ -e "$GITHUB_WORKSPACE" ]] && rm -rf $GITHUB_WORKSPACE/project/project $GITHUB_WORKSPACE/project/target $GITHUB_WORKSPACE/target
+
+# Remove snapshots that might have been cached
+find ~/.nuget/packages -name "*-snapshot" -exec rm -rf {} \;
+find ~/.ivy2/cache -name "*-SNAPSHOT" -exec rm -rf {} \;
+rm -rf ~/.ivy2/local
 
 exit 0
 
