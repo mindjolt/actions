1cff1207d9ea4c49e7bb0b332236f9c8b48668d9
Michael Sandrof <msandrof@jamcity.com>
2021-05-05 19:40:51 -0700

    Revamped cleanup remove files created by the docker user instead of using SBT. Lots faster.

--- sbt-unity/cleanup.sh
+++ sbt-unity/cleanup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Give sbt a shot first
-sbt clean
-
 # Remove build artifacts to prevent some permissioning issues
-[[ -e "$GITHUB_WORKSPACE" ]] && rm -rf $GITHUB_WORKSPACE/project/project $GITHUB_WORKSPACE/project/target $GITHUB_WORKSPACE/target
+[[ -e "$GITHUB_WORKSPACE" ]] && find "$GITHUB_WORKSPACE" -uid $(id -u) -exec rm -r {} \; >/dev/null 2>&1
 
 # Remove snapshots that might have been cached
-find ~/.nuget/packages -name "*-snapshot" -exec rm -rf {} \;
-find ~/.ivy2/cache -name "*-SNAPSHOT" -exec rm -rf {} \;
+find ~/.nuget/packages -name "*-snapshot" -exec rm -rf {} \; >/dev/null 2>&1
+find ~/.ivy2/cache -name "*-SNAPSHOT" -exec rm -rf {} \; >/dev/null 2>&1
 rm -rf ~/.ivy2/local
 
 exit 0
