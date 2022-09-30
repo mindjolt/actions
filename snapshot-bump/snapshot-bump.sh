e5caf96808a819582570b452abb315903a1c6c89
Mat Pataki <mat@jamcity.com>
2022-09-29 22:53:25 -0400

    fix: consume input env vars from github directly in script

--- snapshot-bump/snapshot-bump.sh
+++ snapshot-bump/snapshot-bump.sh
@@ -5,9 +5,10 @@ function help {
   echo " "
   echo "Bumps some semver style version property to the next SNAPSHOT version."
   echo " "
-  echo "--file, -f            the properties file that the version is in. default: gradle.properties"
-  echo "--prop, -p            the version property that will be bumped. default: packageVersion"
-  echo "--help, -g            show this help blurb"
+  echo "--file, -f, env var: INPUT_FILE         the properties file that the version is in. default: gradle.properties"
+  echo "--prop, -p, env var: INPUT_PROPERTY     the version property that will be bumped. default: packageVersion"
+  echo ""
+  echo "--help, -h                              show this help blurb"
   exit 0
 }
 
@@ -20,12 +21,12 @@ while test $# -gt 0; do
       ;;
     -f|--file)
       shift
-      export FILE=$1
+      export INPUT_FILE=$1
       shift
       ;;
     -p|--prop)
       shift
-      PROP=$1
+      INPUT_PROPERTY=$1
       shift
       ;;
     *)
@@ -35,18 +36,18 @@ while test $# -gt 0; do
   esac
 done
 
-if [ -z $FILE ]; then
-  FILE=gradle.properties
+if [ -z $INPUT_FILE ]; then
+  INPUT_FILE=gradle.properties
   echo "using default file: gradle.properties"
 fi
 
-if [ -z $PROP ]; then
-  PROP=packageVersion
+if [ -z $INPUT_PROPERTY ]; then
+  INPUT_PROPERTY=packageVersion
   echo "using default property: packageVersion"
 fi
 
-if ! [[ `grep $PROP $FILE` ]]; then
-  echo "property $PROP not found in file $FILE. bailing"
+if ! [[ `grep $INPUT_PROPERTY $INPUT_FILE` ]]; then
+  echo "property $INPUT_PROPERTY not found in file $INPUT_FILE. bailing"
   help;
 fi
 
