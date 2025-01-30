7b7b4d7b9724649a2c978f327bf0301ecd58e732
Hernan Gonzalez <hgonzalez@jamcity.com>
2025-01-30 17:35:52 -0300

    feat(GS-20988): NuGet - do not publish snapshots

--- nuget-publish-github/publish.sh
+++ nuget-publish-github/publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check if user and password are provided, otherwise use secrets
 [[ -n "$INPUT_USER" ]] && user="$INPUT_USER" || user="$SECRETS_GITHUB_USER"
 [[ -n "$INPUT_PASSWORD" ]] && password="$INPUT_PASSWORD" || password="$SECRETS_GITHUB_TOKEN"
 
@@ -10,5 +11,13 @@ dotnet nuget add source --username "$user" --password "$password" --store-passwo
 path="$INPUT_PACKAGEPATH"
 [[ ! -e "$path" ]] && [[ "${path}" == *"+"* ]] && path="${path%+*}.nupkg"
 
-# Push the package to GitHub NuGet registry
-dotnet nuget push "${path}" --source "github"
+# Extract PACKAGE_VERSION from INPUT_PACKAGEPATH
+PACKAGE_VERSION=$(basename "$path" | cut -d'.' -f3)
+
+# Check if the package version is a snapshot
+if [[ "$PACKAGE_VERSION" == *"SNAPSHOT"* ]]; then
+  echo "Snapshot version detected, skipping publish."
+else
+  # Push the package to GitHub NuGet registry
+  dotnet nuget push "${path}" --source "github"
+fi
